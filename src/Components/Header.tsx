8e1d2fb4804bf490e13318986c0cc10e652c9113
Jamilly Cuco <jamillysantos2507@gmail.com>
2021-08-04 11:21:05 -0300

    header styles with material UI

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,21 +1,54 @@
-import '../styles/header.scss';
 import { setIsModalOpen } from '../Redux/modalSlice';
 import { useAppDispatch } from '../Redux/hooks';
+import { makeStyles, Typography, Box, Button} from '@material-ui/core';
 
 export function Header() {
     const dispatch = useAppDispatch();
+    const styles = useStyles();
 
     return (
         <>
-            <header>
-                <span>O verdadeiro poder está na sua equipe</span>
-                <strong>
+            <Box className={styles.header}>
+                <Typography variant="body2" className={styles.text}>O verdadeiro poder está na sua equipe</Typography>
+                <Typography variant="body1" className={styles.textBold}>
                     Gerencie seus funcionários, especialmente os atendentes
-                </strong>
-            </header>
-            <button className="new-employee" onClick={() => dispatch(setIsModalOpen(true))}>
+                </Typography>
+            </Box>
+            <Button className={styles.button}
+                variant="contained" 
+                color="primary" 
+                disableElevation
+                onClick={() => dispatch(setIsModalOpen(true))}
+                >
                 NOVO FUNCIONÁRIO
-            </button>
+            </Button>  
         </>
     );
 }
+
+const useStyles = makeStyles(() => ({ 
+    header: {
+        background: '#06061e',
+        display: 'flex',
+        flexDirection: 'column',
+        margin: 15,
+        padding: 20,
+        paddingBottom: 30,
+        paddingTop: 30,
+        borderRadius: 5,
+        color: '#FFF',
+    },
+    text: {
+        opacity: 1,
+        marginBottom: 10,
+    },
+    textBold: {
+        fontWeight: 500,
+        opacity: 1,
+    },
+    button: {
+        position: 'absolute',
+        top: 130,
+        right: 22,
+    },
+}))
